55170452afde82c7753ddbf7154f1bbccf7c2fe7
YauhenTaishyn <yauhen1990@gmail.com>
2021-02-04 18:50:17 +0300

    people

--- src/Containers/People/People.js
+++ src/Containers/People/People.js
@@ -4,7 +4,9 @@ import React from 'react'
 const People = () => {
     return (
         <div>
-            <button onClick={People}>People</button>
+            <button onClick={People}>People3</button>
+            <button onClick={People}>People2</button>
+            <button onClick={People}>People1</button>
         </div>
 
     )
