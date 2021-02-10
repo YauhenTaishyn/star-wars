c5b94c0ff8b9d47934e6d61c1f131995a73ce462
YauhenTaishyn <yauhen1990@gmail.com>
2021-02-10 19:51:07 +0300

    nccxc

--- src/Containers/People/People.js
+++ src/Containers/People/People.js
@@ -1,38 +1,33 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import axios from "axios";
 
  const People = () => {
 
 
-     // async function People (ContentType) {
-     //     let url = 'http https://swapi.dev/api/people/1/'
-     //
-     //     let promise = fetch(url, {})
-     //
-     //     let results = await Promise.get(promise)
-     //     console.log(results)
-     // }
+     async function people (ContentType) {
+         let url = 'https://swapi.dev/api/people/'
 
-    let auth = (name, birth_year, eye_color) => {
 
-         return async () => {
+         let results = await fetch(url)
 
-             const authData = {
-                 name,
-                 birth_year,
-                 eye_color
-             }
+         let data = await results.json()
 
-             let url= `http https://swapi.dev/api/people/1/`
-
-             const response = await axios.post(url, authData)
-             console.log(response)}
 
      }
 
+
+   // useEffect(()=>{
+   //     (async function() {
+   //         let url= `https://swapi.dev/api/people/`
+   //
+   //         const response = await axios.get(url)
+   //         console.log(response)
+   //
+   //     })()
+   // })
      return (
          <div>
-             <button onClick={People}>People</button>
+             <button onClick={people}>People</button>
 
          </div>
 
@@ -40,50 +35,6 @@ import axios from "axios";
 }
 export default People
 
-// export const auth = (name, birth_year, eye_color) => {
-//
-//     return async () => {
-//
-//          const authData = {
-//              name,
-//              birth_year,
-//              eye_color
-//         }
-//
-//         let url= `http https://swapi.dev/api/people/1/`
-//
-//         const response = await axios.post(url, authData)
-//        console.log(response)}
-//
-//  }
-//
-
-
-
-
-// async function getUsers(names) {
-//     let jobs = [];
-//
-//     for(let name of names) {
-//         let job = fetch(`https://api.github.com/users/${name}`).then(
-//             successResponse => {
-//                 if (successResponse.status != 200) {
-//                     return null;
-//                 } else {
-//                     return successResponse.json();
-//                 }
-//             },
-//             failResponse => {
-//                 return null;
-//             }
-//         );
-//         jobs.push(job);
-//     }
-//
-//     let results = await Promise.all(jobs);
-//
-//     return results;
-// }
 
 
 
@@ -92,11 +43,6 @@ export default People
 
 
 
-// let response = await fetch(url);
-//
-// if (response.ok) { // если HTTP-статус в диапазоне 200-299
-//                    // получаем тело ответа (см. про этот метод ниже)
-//     let json = await response.json();
-// } else {
-//     alert("Ошибка HTTP: " + response.status);
-// }
+
+
+
